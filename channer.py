4ff85b456a54073f1f7e65e549eb40c6e3f1e4d9
xchek <25627173+xchek@users.noreply.github.com>
2019-12-28 06:59:57 -0500

    add changes

--- channer.py
+++ channer.py
@@ -1,17 +1,16 @@
-import requests, re, sys
+import requests, re, humanize, click, time, random
 from bs4 import BeautifulSoup as bs
 from pathlib import Path
 from urllib.parse import urljoin
 from pydash import arrays as _arrays
-import humanize
-import argparse
-import click
+from fake_useragent import UserAgent
 
-
+# TODO: Functionality to specify filetypes to fetch
 specific_filetypes = ["jpg", "png", "webm", "gif"]
+ua = UserAgent()
 s = requests.Session()
 s.headers.update({
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
+  'User-Agent': ua.random,
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'Accept-Encoding': 'gzip, deflate, br',
   'Upgrade-Insecure-Requests': '1'})
@@ -19,13 +18,26 @@ s.verify = False
 dl_ct, total_dld = 0, 0
 
 @click.command()
-@click.option('-o', '--output-dir', 'output_parent_dir', default='.')
+@click.option('-o', '--output-dir', 'output_parent_dir',
+              default='.',
+              help='Specify the folder to create the output.')
+@click.option('-w', '--wait', 'wait_time',
+              default=None, nargs=2, type=int,
+              help='Specify a range (in seconds) to wait between initiating each download.')
 @click.argument('url')
-def execute(url, output_parent_dir):
+
+def scrape_channel(url, output_parent_dir, wait_time):
+  """
+  :param url: [str] -
+
+  :param wait_time: Optional[(A, B)] - A random time to wait in seconds between A and B.
+
+  :param output_parent_dir: Optional[str] - Specified path in which to make the output.
+
+  """
   global total_dld, dl_ct
   input_url = url
   output_dir_name = input_url.replace("://", "_").replace(".", "_").replace("/", "-")
-  print(output_parent_dir)
   output_parent = Path(output_parent_dir)
   output_dir = output_parent / output_dir_name
   output_dir.mkdir(exist_ok=True)
@@ -35,7 +47,7 @@ def execute(url, output_parent_dir):
   links = get_links(input_url, soup)
   print(f"Attempting to download {len(links)} things!")
   try:
-    dls = _arrays.compact(list(map(lambda x: download_file(x, path=output_dir), links)))
+    dls = _arrays.compact(list(map(lambda x: download_file(x, path=output_dir, waiting_time=wait_time), links)))
   except:
     print("Stopping!")
   finally:
@@ -54,7 +66,7 @@ def get_links(url, soup):
     if href and href not in lns: lns.append(href)
   return list(set(lns))
 
-def download_file(url, path="."):
+def download_file(url, waiting_time=None, path="."):
   global total_dld, dl_ct
   local_filename = url.split("/")[-1]
   ext = local_filename.split(".")[-1]
@@ -65,6 +77,7 @@ def download_file(url, path="."):
   ext_path.mkdir(exist_ok=True)
   local_path = ext_path / local_filename
   if not local_path.is_file():
+    if waiting_time: time.sleep(random.uniform(*waiting_time))
     with s.get(url, stream=True) as resp:
       try:
         cl = int(resp.headers["content-length"])
@@ -79,9 +92,9 @@ def download_file(url, path="."):
           if chunk:
             f.write(chunk)
     dl_ct += 1
-    return local_filename
+    return str(local_path)
   else:
     print(f"Already downloaded {local_filename}! Skipping.")
 
 if __name__ == "__main__":
-  execute()
+  scrape_channel()
